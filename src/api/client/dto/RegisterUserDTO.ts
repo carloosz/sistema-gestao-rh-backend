263df5b4480e2522c23d22ba73e2168a6f6eaa02
Carlos Henrique <carlitossilva100@gmail.com>
2025-09-05 00:26:15 -0300

    feat: RegisterUserDTO

---
+++ src/api/client/dto/RegisterUserDTO.ts
@@ -0,0 +1,28 @@
+interface RegisterUserDTO {
+    email: string;
+    password: string;
+    name: string;
+    cpf: string;
+    phone: string;
+    dateOfBirth: string;
+    gender: string;
+    zipCode: string;
+    street: string;
+    number: string;
+    neighborhood: string;
+    city: string;
+    state: string;
+    admissionDate: string;
+    Cbo: string;
+    startingSalary: number;
+    natureOfThePosition: string;
+    sector: string;
+    paymentMethod: string;
+    initialHour: string;
+    finalHour: string;
+    lunchInitialHour: string;
+    lunchFinalHour: string;
+    daysOfWork: string[];
+}
+
+export { RegisterUserDTO };
