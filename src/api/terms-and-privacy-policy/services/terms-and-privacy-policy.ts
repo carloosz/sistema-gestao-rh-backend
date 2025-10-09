5dc4fd1919e339d126c9d78d78d7eeeee833378c
Carlos Henrique <carlitossilva100@gmail.com>
2025-10-09 00:37:48 -0300

    feat: TermsAndPrivacyPolicy entity

---
+++ src/api/terms-and-privacy-policy/services/terms-and-privacy-policy.ts
@@ -0,0 +1,7 @@
+/**
+ * terms-and-privacy-policy service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::terms-and-privacy-policy.terms-and-privacy-policy');
